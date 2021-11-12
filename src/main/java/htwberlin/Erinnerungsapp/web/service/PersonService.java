96eefd1667ab1b98f8b506374d28e3293f84d7cf
sinan <s0576403@htw-berlin.de>
2021-11-12 18:54:51 +0100

    Ressource angelegt

---
+++ src/main/java/htwberlin/Erinnerungsapp/web/service/PersonService.java
@@ -0,0 +1,46 @@
+package htwberlin.Erinnerungsapp.web.service;
+
+import htwberlin.Erinnerungsapp.web.api.Person;
+import htwberlin.Erinnerungsapp.web.api.PersonCreateRequest;
+import htwberlin.Erinnerungsapp.web.persistence.PersonEntity;
+import htwberlin.Erinnerungsapp.web.persistence.PersonRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public List<Person> findAll() {
+        List<PersonEntity> persons = personRepository.findAll();
+        return persons.stream()
+                .map(this::transformEntity)
+                .collect(Collectors.toList());
+
+    }
+
+    public Person create(PersonCreateRequest request) {
+        var personEntity = new PersonEntity(request.getFirstName(), request.getLastName(), request.isVaccinated());
+        personEntity = personRepository.save(personEntity);
+        return transformEntity(personEntity);
+    }
+
+    private Person transformEntity(PersonEntity personEntity) {
+        return new Person(
+                personEntity.getId(),
+                personEntity.getFirstName(),
+                personEntity.getLastName(),
+                personEntity.isVaccinated()
+        );
+
+
+
+    }
+}
--- src/main/java/htwberlin/Erinnerungsapp/web/web2/PersonRestController.java
+++ src/main/java/htwberlin/Erinnerungsapp/web/web2/PersonRestController.java
@@ -2,28 +2,42 @@ package htwberlin.Erinnerungsapp.web.web2;
 
 
 import htwberlin.Erinnerungsapp.web.api.Person;
+import htwberlin.Erinnerungsapp.web.api.PersonCreateRequest;
+import htwberlin.Erinnerungsapp.web.service.PersonService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
 public class PersonRestController {
 
-    private List<Person> persons;
+    private final PersonService personService;
 
-    public PersonRestController() {
-        persons = new ArrayList<>();
-        persons.add(new Person(1,"Max","Mustermann",false));
-        persons.add(new Person(2,"Maxi","Meier",true));
-
+    public PersonRestController(PersonService personService) {
+        this.personService = personService;
     }
+
 
     @GetMapping(path = "/api/v1/persons")
     public ResponseEntity<List<Person>> fetchPersons(){
-        return ResponseEntity.ok(persons);
+        return ResponseEntity.ok(personService.findAll());
 
     }
+
+    @PostMapping(path = "/api/v1/persons")
+    public ResponseEntity<Void> createPerson(@RequestBody PersonCreateRequest request) throws URISyntaxException {
+        var person= personService.create(request);
+        URI uri = new URI("/api/v1/persons/"+ person.getId());
+        return ResponseEntity.created(uri).build();
+
+
+    }
+
+
 }
