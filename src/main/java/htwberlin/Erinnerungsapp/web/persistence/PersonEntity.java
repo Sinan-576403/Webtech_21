e89d1ed0e14699e5bb97021cfa3831377437fa21
sinan <s0576403@htw-berlin.de>
2021-12-02 17:48:23 +0100

    Columns

--- src/main/java/htwberlin/Erinnerungsapp/web/persistence/PersonEntity.java
+++ src/main/java/htwberlin/Erinnerungsapp/web/persistence/PersonEntity.java
@@ -2,21 +2,21 @@ package htwberlin.Erinnerungsapp.web.persistence;
 
 import javax.persistence.*;
 
-@Entity(name = "persons")
+@Entity(name = "Erinnerungen")
 public class PersonEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "Wichtigkeit")
     private long id;
 
-    @Column(name = "first_name", nullable = false)
+    @Column(name = "Aufgabe_1", nullable = false)
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
+    @Column(name = "Aufgabe_2", nullable = false)
     private String lastName;
 
-    @Column(name = "is_vaccinated")
+    @Column(name = "ist_erledigt")
     private boolean vaccinated;
 
     public PersonEntity(String firstName, String lastName, boolean vaccinated) {
