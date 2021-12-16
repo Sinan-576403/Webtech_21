4442dcbaff43e009cc6884bc7278f293d8ea1cb3
sinan <s0576403@htw-berlin.de>
2021-12-16 13:31:19 +0100

    Unbenennung

---
+++ src/main/java/htwberlin/Erinnerungsapp/web/service/ErinnerungService.java
@@ -0,0 +1,78 @@
+package htwberlin.Erinnerungsapp.web.service;
+
+import htwberlin.Erinnerungsapp.web.api.Erinnerung;
+import htwberlin.Erinnerungsapp.web.api.ErinnerungManipulationRequest;
+import htwberlin.Erinnerungsapp.web.persistence.ErinnerungEntity;
+import htwberlin.Erinnerungsapp.web.persistence.ErinnerungRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ErinnerungService {
+
+    private final ErinnerungRepository erinnerungRepository;
+
+    public ErinnerungService(ErinnerungRepository erinnerungRepository) {
+        this.erinnerungRepository = erinnerungRepository;
+    }
+
+    public List<Erinnerung> findAll() {
+        List<ErinnerungEntity> erinnerungen = erinnerungRepository.findAll();
+        return erinnerungen.stream()
+                .map(this::transformEntity)
+                .collect(Collectors.toList());
+
+    }
+
+    public Erinnerung findById(Long id){
+        var erinnerungEntity = erinnerungRepository.findById(id);
+        return erinnerungEntity.map(this::transformEntity).orElse(null);
+    }
+
+    public Erinnerung create(ErinnerungManipulationRequest request) {
+        var erinnerungEntity = new ErinnerungEntity(request.getErsteAufgabe(), request.getNachsteAufgabe(), request.isErledigt());
+        erinnerungEntity = erinnerungRepository.save(erinnerungEntity);
+        return transformEntity(erinnerungEntity);
+    }
+
+    public Erinnerung update(Long id, ErinnerungManipulationRequest request) {
+        var erinnerungEntityOptional = erinnerungRepository.findById(id);
+        if (erinnerungEntityOptional.isEmpty()) {
+            return null;
+        }
+
+        var erinnerungEntity = erinnerungEntityOptional.get();
+        erinnerungEntity.setErsteAufgabe(request.getErsteAufgabe());
+        erinnerungEntity.setNachsteAufgabe(request.getNachsteAufgabe());
+        erinnerungEntity.setErledigt(request.isErledigt());
+        erinnerungEntity = erinnerungRepository.save(erinnerungEntity);
+
+        return transformEntity(erinnerungEntity);
+    }
+
+    public boolean deleteById(Long id){
+        if (!erinnerungRepository.existsById(id)){
+            return false;
+        }
+        erinnerungRepository.deleteById(id);
+        return true;
+
+
+    }
+
+
+
+    private Erinnerung transformEntity(ErinnerungEntity erinnerungEntity) {
+        return new Erinnerung(
+                erinnerungEntity.getId(),
+                erinnerungEntity.getErsteAufgabe(),
+                erinnerungEntity.getNachsteAufgabe(),
+                erinnerungEntity.isErledigt()
+        );
+
+
+
+    }
+}
---
+++ src/main/java/htwberlin/Erinnerungsapp/web/web2/ErinnerungRestController.java
@@ -0,0 +1,57 @@
+package htwberlin.Erinnerungsapp.web.web2;
+
+
+import htwberlin.Erinnerungsapp.web.api.Erinnerung;
+import htwberlin.Erinnerungsapp.web.api.ErinnerungManipulationRequest;
+import htwberlin.Erinnerungsapp.web.service.ErinnerungService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+public class ErinnerungRestController {
+
+    private final ErinnerungService erinnerungService;
+
+    public ErinnerungRestController(ErinnerungService erinnerungService) {
+        this.erinnerungService = erinnerungService;
+    }
+
+
+    @GetMapping(path = "/api/v1/Erinnerungen")
+    public ResponseEntity<List<Erinnerung>> fetcherinnerungen(){
+        return ResponseEntity.ok(erinnerungService.findAll());
+
+    }
+
+    @GetMapping(path = "/api/v1/Erinnerungen/{id}")
+    public ResponseEntity<Erinnerung>fetchErinnerungById(@PathVariable Long id){
+        var erinnerung = erinnerungService.findById(id);
+        return erinnerung != null? ResponseEntity.ok(erinnerung): ResponseEntity.notFound().build();
+    }
+
+    @PostMapping(path = "/api/v1/Erinnerungen")
+    public ResponseEntity<Void> createErinnerung(@RequestBody ErinnerungManipulationRequest request) throws URISyntaxException {
+        var erinnerung= erinnerungService.create(request);
+        URI uri = new URI("/api/v1/Erinnerungen/"+ erinnerung.getId());
+        return ResponseEntity.created(uri).build();
+
+
+    }
+    @PutMapping(path = "/api/v1/Erinnerungen/{id}")
+    public ResponseEntity<Erinnerung> updateErinnerung(@PathVariable Long id, @RequestBody ErinnerungManipulationRequest request) {
+        var erinnerung = erinnerungService.update(id, request);
+        return erinnerung != null? ResponseEntity.ok(erinnerung): ResponseEntity.notFound().build();
+
+    }
+
+    @DeleteMapping(path = "/api/v1/Erinnerungen/{id}")
+    public ResponseEntity<Void> deleteErinnerung(@PathVariable Long id){
+        boolean successful = erinnerungService.deleteById(id);
+        return successful? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+    }
+
+}
